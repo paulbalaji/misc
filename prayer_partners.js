31ae8d1ad86cb6e3eb4c16cd7833dd2163b4a270
Paul Balaji <pmb14@ic.ac.uk>
2016-02-08 00:36:14 +0000

    made program generate rota for a year
    Signed-off-by: Paul Balaji <pmb14@ic.ac.uk>

--- prayer_partners.js
+++ prayer_partners.js
@@ -1,7 +1,10 @@
 var fs = require('fs');
 
-// pass in the offset when calling the program
-var offset = process.argv[2];
+/*
+   OLD VERSION:
+    - pass in the offset when calling the program, default 0
+    - var offset = process.argv.length > 2 ? process.argv[2] : 0;
+*/
 
 // read in data from names.txt
 // put names into a list
@@ -11,9 +14,20 @@ var families = fs.readFileSync('names.txt').toString().split("\n")
 var right = families;
 var left = right.splice(0, families.length / 2);
 
-// generate pairs
-for (j in left) {
-    var x = left[j];
-    var y = right[(j + offset) % right.length];
-    console.log(x + " PAIRED WITH " + y);
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+              'September', 'October', 'November', 'December'];
+
+for (month in months) {
+    console.log("ROTA FOR " + months[month] + ":");
+    generatePairs(month);
+    console.log("\n\n");
+}
+
+function generatePairs(offset) {
+    for (j in left) {
+        var x = left[j];
+        var y = right[(j + offset) % right.length];
+        console.log(x + " PAIRED WITH " + y);
+    }
 }
